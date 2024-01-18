9fb48d20e6256a848ef39150c75338b81de1c0bc
DilnaDilin <dilna.dilin7@gmail.com>
2024-01-18 15:21:31 +0400

    'completed'

--- websitescrapproject/webscrapapp/views.py
+++ websitescrapproject/webscrapapp/views.py
@@ -1,16 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 import requests
 from bs4 import BeautifulSoup
+
+
 from .models import Link
 # Create your views here.
 def home(request):
-    urls=requests.get('https://www.google.com/')
-    beautifulsoup=BeautifulSoup(urls.text,'html.parser')
-    address=[]
-    for link in beautifulsoup.find_all('a'):
-        address=link.get('href')
-        name=link.name
-        Link.objects.create(name=name, address=address)
-    data_values=Link.objects.all()
+    if request.method=='POST':
+        site=request.POST.get('addressurl','')
+        urls=requests.get(site)
+        print(urls)
+        beautifulsoup=BeautifulSoup(urls.text,'html.parser')
+        address=[]
+        for link in beautifulsoup.find_all('a'):
+            address=link.get('href')
+            name=link.name
+            Link.objects.create(name=name, address=address)
+        data_values=Link.objects.all()
+        return redirect('/')
+    data_values = Link.objects.all()
     return render(request,'home.html',{'data_values':data_values})
+def clear(request):
+    Link.objects.all().delete()
+    return redirect('/')
