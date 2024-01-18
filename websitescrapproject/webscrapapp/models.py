1585b553afb75282dc59687cd7115890b1c3d13f
DilnaDilin <dilna.dilin7@gmail.com>
2024-01-18 12:55:48 +0400

    'initialwithstaticsite'

--- websitescrapproject/webscrapapp/models.py
+++ websitescrapproject/webscrapapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Link(models.Model):
+    name=models.CharField(max_length=500,null=True,blank=True)
+    address=models.CharField(max_length=500,null=True,blank=True)
+    def __str__(self):
+        return self.name
--- websitescrapproject/webscrapapp/views.py
+++ websitescrapproject/webscrapapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+import requests
+from bs4 import BeautifulSoup
 # Create your views here.
+def home(request):
+    urls=requests.get('https://www.google.com/')
+    beautifulsoup=BeautifulSoup(urls.text,'html.parser')
+    address=[]
+    for link in beautifulsoup.find_all('a'):
+        address.append(link.get('href'))
+        #name=link.name
+    return render(request,'home.html',{'address':address})
